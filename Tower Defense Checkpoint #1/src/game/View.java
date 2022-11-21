a9f3bc2edf5dfecd14783f65e8b87f86fc206b0e
Bruce Crockett <brucrocrockett@gmail.com>
2022-11-21 13:17:30 -0700

    This is a test commit from Bruce's Eclipse directory.

--- Tower Defense Checkpoint #1/src/game/View.java
+++ Tower Defense Checkpoint #1/src/game/View.java
@@ -9,9 +9,6 @@ package game;
 
 import java.awt.Dimension;
 import java.awt.Graphics;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
