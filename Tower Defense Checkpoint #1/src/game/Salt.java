1a2bffdeea7262cacbe09f39f79fdc827427e3cd
Logan Luker <loganwluker@gmail.com>
2022-11-22 14:05:15 -0700

    New Salt Tower Class

---
+++ Tower Defense Checkpoint #1/src/game/Salt.java
@@ -0,0 +1,19 @@
+package game;
+
+import java.awt.Graphics;
+
+public class Salt extends GameObject {
+
+	@Override
+	public void update(double elapsedTime) 
+	{
+		
+	}
+
+	@Override
+	public void draw(Graphics g) 
+	{
+		
+	}
+
+}
